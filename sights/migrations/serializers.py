3e89fe547d33f64946e0f97b0efbc1cf2aa986c6
thoma <thomai_kastoria@hotmail.com>
2024-06-12 08:37:39 +0300

    initial

---
+++ sights/migrations/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from sights.models import sights, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = sights
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+
+
+class UserSerializer(sights.ModelSerializer):
+    sights = serializers.PrimaryKeyRelatedField(many=True, queryset=sights.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'sights']
+
