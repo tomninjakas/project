2097978ceb0e470e37c1255db2ec910ea8c286fe
thoma <thomai_kastoria@hotmail.com>
2024-06-14 10:59:31 +0300

    initial14_06

--- sights/models.py
+++ sights/models.py
@@ -7,15 +7,15 @@ CATEGORY_CHOICES = [('nature', 'NATURE'), ('church', 'CHURCH'), ('museum', 'MUSE
 
 class Sights(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TextField()
-    linenos = models.BooleanField(default=False)
-    Category = models.CharField(choices=CITY_CHOICES, default='kastoria', max_length=100)
-    style = models.CharField(choices=CATEGORY_CHOICES, default='nature', max_length=100)
+    Title = models.CharField(max_length=100, blank=True, default='')
+    Image = models.ImageField(max_length=100, blank=True, default='')
+    Information = models.TextField()
+    City = models.CharField(choices=CITY_CHOICES, default='kastoria', max_length=100)
+    Category = models.CharField(choices=CATEGORY_CHOICES, default='nature', max_length=100)
     owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE, null=True)
 
     class Meta:
         ordering = ['created']
 
     def __str__(self):
-        return self.title
+        return self.Title
